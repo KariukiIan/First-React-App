e6895baaa348fb9fe2fe7177b173f643739fa849
Ian Kariuki <kariukiian816@gmail.com>
2024-11-21 10:22:58 +0300

    Added focus to input add item element after the user has added a product to the list

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
-
-
+import { useRef } from "react";
 
 const AddProduct = ({ newProduct, setNewProduct, handleSubmit }) => {
+	const inputRef = useRef();
+
 	return (
 		<form className="addForm" onSubmit={handleSubmit}>
 			<label htmlFor="addItem"></label>
 			<input
 				type="text"
 				autoFocus
+				ref={inputRef}
 				id="addItem"
 				placeholder="add Item"
 				required
 				value={newProduct}
 				onChange={(e) => setNewProduct(e.target.value)}
 			/>
-			<button type="submit" aria-label="Add Item">
+			<button
+				type="submit"
+				aria-label="Add Item"
+				onClick={() => inputRef.current.focus()}
+			>
 				<FaPlus />
 			</button>
 		</form>
-	); 
+	);
 };
 
 export default AddProduct;
